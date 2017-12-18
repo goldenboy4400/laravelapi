117625d571439bcabd6d394df297704b59945af2
vagrant <vagrant@homestead>
2017-12-18 18:29:14 +0000

    product resource added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
@@ -14,6 +16,8 @@ class ProductController extends Controller
      */
     public function index()
     {
+
+        return Product::all();
         //
     }
 
@@ -46,6 +50,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
+        return new ProductResource($product);
+
         //
     }
 
