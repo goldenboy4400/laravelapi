121e81fa017253fcb3b9c6307db179e87162584c
vagrant <vagrant@homestead>
2017-12-18 18:44:45 +0000

    modify product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,17 @@ class ProductResource extends Resource
             'name'=>$this->name,
             'description'=> $this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+
+            'stock'=>$this->stock ==0 ? 'Out of stock':$this->stock,
+
+            'discount'=>$this->discount,
+            'totalPrice'=>round((1-($this->discount/100)) * $this->discount,2),
+            'rating'=> $this->reviews->count() >0 ? round($this->reviews->sum('star')/$this->reviews->count(),2):
+                'no rating yet',
+
+
+            'href'=>['reviews'=> route('reviews.index', $this->id)
+            ]
         ];
     }
 }
